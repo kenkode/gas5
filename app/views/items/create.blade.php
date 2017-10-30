7f075de8db2c01496523a854e25d2ef8755adad8
kenkode <wangoken2@gmail.com>
2017-10-30 16:18:46 +0300

    size

--- app/views/items/create.blade.php
+++ app/views/items/create.blade.php
@@ -32,7 +32,7 @@
         </div>
         <div class="form-group">
             <label for="username">Item Size (Kg) <span style="color:red">*</span> :</label>
-            <input class="form-control" placeholder="" type="text" name="item_size" id="item_size" value="{{{ Input::old('item_size') }}}" required>
+            <input class="form-control" placeholder="" type="text" name="item_size" id="item_size" value="{{{ Input::old('item_size') }}}">
         </div>
 
          <div class="form-group">
