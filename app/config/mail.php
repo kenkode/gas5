746c8e0293c92e0010cd624beb2d003edfd56246
kenkode <wangoken2@gmail.com>
2017-11-09 18:14:27 +0300

    price

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'mail.gx.co.ke',
+	'host' => 'mail.lixnet.net',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'info@gx.co.ke', 'name' => 'Gas Express'), 
+	'from' => array('address' => 'info@lixnet.net', 'name' => 'Gas Express'), 
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'info@gx.co.ke',
+	'username' => 'info@lixnet.net',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'gasexpress2017',
+	'password' => 'lixnetco2015',
 
 	/*
 	|--------------------------------------------------------------------------
